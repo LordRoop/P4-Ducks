2324cf196915e6c3ba22c213228aa72e5929c702
LordRoop <jagroopvij@gmail.com>
2020-12-16 12:27:56 -0800

    changed images

--- main.py
+++ main.py
@@ -19,29 +19,7 @@ def journals():
     return render_template("journals.html")
 
 
-@app.route('/pedro')
-def pedro():
-    return render_template("pedro.html")
 
-
-@app.route('/roop')
-def roop():
-    return render_template("roop.html")
-
-
-@app.route('/arul')
-def arul():
-    return render_template("arul.html")
-
-
-@app.route('/manuel')
-def manuel():
-    return render_template("manuel.html")
-
-
-@app.route('/colin')
-def colin():
-    return render_template("colin.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port=' 5000', host='127.0.0.1')
