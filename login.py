145c46a0f6e480eb8df80dac6ba7dd77897d4fa3
Akshit Prathipati <akprathipati@gmail.com>
2021-02-18 22:59:13 -0800

    Added get and post

--- login.py
+++ login.py
@@ -1,11 +1,21 @@
 import sqlite3
 
-from flask import redirect , render_template
-from create import checkLogin
+from flask import redirect
+
 
 def validate(request):
-    if checkLogin(request) == 1:
-        return redirect('/')
-    else:
-        return render_template("login.html", error='UserId or password Incorrect')
+    user = request.form['user']
+    password = request.form['user_pass']
+    print("Username" + "\t" + user + "\t" + "Password" + "\t" + password + "\t" + "Email")
+    userinfo = [user, password, mail]
+    conn = sqlite3.connect('user.db')
+    # Creating a Cursor
+    c = conn.cursor()
+    c.execute("INSERT INTO users VALUES (?,?,?)", userinfo)
+    # Commit our command
+    conn.commit()
 
+    # close our connection
+    conn.close()
+
+    return redirect('/')
